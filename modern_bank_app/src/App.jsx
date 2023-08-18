24a4972abcbcf43b4660a67c44b52e7eb64fc7fe
Andhika_RK <andhikarestu873@gmail.com>
2023-08-18 21:16:57 +0700

    finished product

--- modern_bank_app/src/App.jsx
+++ modern_bank_app/src/App.jsx
@@ -11,13 +11,13 @@ const App = () => (
       </div>
 
       <div className={`bg-primary ${styles.flexStart}`}>
-        <div className={`$${styles.boxWidth}`}>
+        <div className={`${styles.boxWidth}`}>
           <Hero />
         </div>
       </div>
 
       <div className={`bg-primary ${styles.flexStart} ${styles.paddingX} `}>
-        <div className={`$${styles.boxWidth}`}>
+        <div className={`${styles.boxWidth}`}>
           <Stats />
           <Business />
           <Billing />
